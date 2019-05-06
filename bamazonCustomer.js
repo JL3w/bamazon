3d00f5af2c3d4f9eea9f238c12ec80ff320a2039
J Lew <jtl@metricwise.net>
2019-05-06 18:55:50 -0400

    update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,73 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "whatthefuck",
     database: "bamazon_db"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    
+    start();
 });
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err
+        else console.table(res, "\n");
+        prompt();
+    });
+};
+
+function prompt() {
+    var rows = connection.query("SELECT count(*) FROM products", function (err, res) {
+        if (err) throw err
+        else return res;
+    });
+    inquirer
+        .prompt([
+            {
+            name: "prodID",
+            type: "input",
+            message: "What is the id of the product you would like to purchase?",
+            validate: function(id) {
+                if (id <= rows) {
+                    return true;
+                    }
+                return false;
+                }
+            },
+            {
+            name: "quantity",
+            type: "input",
+            message: "How many units of the product would you like to purchase?",
+            }
+        ]).then(function(answer) {
+            var query = "SELECT * FROM products WHERE item_id = ? and stock_quantity >= ? ";
+            connection.query(query, [answer.prodID, answer.quantity], function(err, res) {
+                if (err) throw err;
+
+                if (res.stock_quantity = null) {
+                    console.log("We do not have that product in that quantity");
+                }
+            
+                else { 
+                    connection.end();
+                    var newQuant = parseInt(res.stock_quantity - answer.quantity);
+                    var total = res.price * answer.quantity;
+                    var query = "UPDATE products SET stock_quantity=? WHERE item_id=?";
+                    
+                    connection.query(query, [newQuant, answer.prodID], function (err, res) {
+                        if (err) throw err;
+                        console.log("Your order has been placed at a total cost of " + total);
+                        });
+                    }
+            });
+            connection.end();
+        });
+};
+
---
+++ seed.sql
@@ -0,0 +1,14 @@
+use bamazon_db;
+
+insert into products (product_name, department_name, price, stock_quantity)
+values 
+("Table" , "Office Supplies", 300, 200), 
+("Chair" , "Office Supplies", 200, 400), 
+("Desk" , "Office Supplies", 400, 100), 
+("Table" , "Office Supplies", 300, 200),  
+("Cell Phone" , "Electronics", 800, 700), 
+("Game Dude" , "Electronics", 250, 900), 
+("Midi Keyboard" , "Instruments", 400, 200), 
+("Sax" , "Instruments", 1000, 100), 
+("Bud" , "Beer", 9, 20000), 
+("La Fin Du Monde" , "Beer", 30, 200);
